cc5b49b2fac712b6e4e91f3b2cacd2afa706a67d
Xavian Moody <XavianMoody1025@gmail.com>
2024-08-04 15:16:18 -0400

    Created hamburger icon and added animation

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,2 +1,55 @@
 "use strict";
-console.log("Hello World");
+
+// Toggle Hamburger Menu
+function toggleHamburgerMenu() {
+  const hamburgerMenu = document.querySelector(".hamburger-icon");
+  const lines = document.querySelectorAll(".hamburger-icon__line");
+  const animationClasses = ["top-line", "middle-line", "bottom-line"];
+  let isOpen = false;
+
+  function addOpenClasses() {
+    lines.forEach((line, index) =>
+      line.classList.add(
+        `hamburger-icon__${animationClasses[index]}--animation`
+      )
+    );
+  }
+
+  function removeOpenClasses(params) {
+    lines.forEach((line, index) =>
+      line.classList.remove(
+        `hamburger-icon__${animationClasses[index]}--animation`
+      )
+    );
+  }
+
+  function addCloseClasses(params) {
+    lines.forEach((line, index) =>
+      line.classList.add(
+        `hamburger-icon__${animationClasses[index]}--animation-reverse`
+      )
+    );
+  }
+
+  function removeCloseClasses(params) {
+    lines.forEach((line, index) =>
+      line.classList.remove(
+        `hamburger-icon__${animationClasses[index]}--animation-reverse`
+      )
+    );
+  }
+
+  hamburgerMenu.addEventListener("click", () => {
+    isOpen = !isOpen;
+
+    if (isOpen) {
+      addOpenClasses();
+      removeCloseClasses();
+    } else {
+      removeOpenClasses();
+      addCloseClasses();
+    }
+  });
+}
+
+toggleHamburgerMenu();
